c80d90359833f6f38a5b5d21621b6b218ceb2d78
kumawatPriya <priyakumawat9868@gmail.com>
2026-03-06 18:27:25 +0530

    fixes

--- src/controllers/document.controller.js
+++ src/controllers/document.controller.js
@@ -1,43 +1,7 @@
 const minioClient = require("../config/minio");
-const { v4: uuidv4 } = require("uuid");
 const db = require("../config/db");
 
 const BUCKET = "jsc-crm";
-
-// exports.uploadEmployeeLetter = async (req, res) => {
-//   try {
-//     const file = req.file;
-//     const { employee_id, document_type } = req.body;
-//     if (!file) {
-//       return res.status(400).json({
-//         message: "PDF file is required"
-//       });
-//     }
-//     const fileName = `employee/letter/${uuidv4()}.pdf`;
-
-//     await minioClient.putObject(  BUCKET, fileName, file.buffer, file.size,
-//       { "Content-Type": "application/pdf" }
-//     );
-
-//     const file_url = `http://103.110.127.211:9000/${BUCKET}/${fileName}`;
-
-//     await db.query(
-//       `INSERT INTO employee_documents 
-//        (employee_id, document_type, file_url)
-//        VALUES (?, ?, ?)`,
-//       [employee_id, document_type, file_url]
-//     );
-
-//     res.json({
-//       message: "Offer letter uploaded successfully",
-//       file_url
-//     }); }
-//      catch (error) {
-//     res.status(500).json({
-//       error: error.message
-//     });
-//   }
-// };
 
 
 exports.uploadEmployeeLetter = async (req, res) => {

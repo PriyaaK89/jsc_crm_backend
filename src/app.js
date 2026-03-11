72a3c3bf1ec4e1245dfd3982ea520f1b9a4a34ce
kumawatPriya <priyakumawat9868@gmail.com>
2026-03-11 16:44:08 +0530

    fixes

--- src/app.js
+++ src/app.js
@@ -5,9 +5,12 @@ require('dotenv').config();
 const app = express();
 
 // Middlewares
+
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 const departmentRoute = require("../src/routes/department.routes");
 const jobRoleRoute = require("../src/routes/jobRole.routes");

31b150d15f71a04136d14d5afb848061646018b4
kumawatPriya <priyakumawat9868@gmail.com>
2026-08-22 19:49:23 +0530

    .

--- src/controllers/visit.controller.js
+++ src/controllers/visit.controller.js
@@ -50,12 +50,12 @@ exports.createVisit = async (req, res) => {
       return res.status(400).json({ message: "Invalid visit purpose" });
     }
 
-     if (isPastVisitCutoff()) {
-      return res.status(400).json({
-        success: false,
-        message: "You can submit a visit only up to 7:00 PM. Please contact your reporting manager if you need to log it late."
-      });
-    }
+    //  if (isPastVisitCutoff()) {
+    //   return res.status(400).json({
+    //     success: false,
+    //     message: "You can submit a visit only up to 7:00 PM. Please contact your reporting manager if you need to log it late."
+    //   });
+    // }
       const attendance = await attendanceModel.getTodayAttendance(user_id);
 
     if (!attendance) {
